024bdf6faf739fdaf667fbc716ca77950e1b79d0
Duhan Avcı <avciduhan@gmail.com>
2025-01-23 18:01:13 +0300

    main menu scene update

--- Assets/Scripts/Menu Scene/TitleScreenManager.cs
+++ Assets/Scripts/Menu Scene/TitleScreenManager.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class TitleScreenManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartNewGame()
     {
-        
-    }
+     
+        StartCoroutine(WorldSaveManager.instance.LoadNewGame());
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/World Managers/WorldSaveManager.cs
+++ Assets/Scripts/World Managers/WorldSaveManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class WorldSaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static WorldSaveManager instance = null;//singleton yapýsý
+
+    [SerializeField] int worldSceneIndex = 1;
+    private void Awake()
     {
-        
+
+        if (instance == null)//bu bu scriptin instance 'ýný ayný anda kullanan sadece 1 tane obje olabilir.
+        {
+            instance = this;
+        }
+        else//eðer hali hazýrda varsa yok et.
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        DontDestroyOnLoad(gameObject); //bu instanceyi scene deðiþtirsekte yok olmamasý için.
     }
+
+    public IEnumerator LoadNewGame()
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(worldSceneIndex);
+        yield return null;
+    }
+    
 }
