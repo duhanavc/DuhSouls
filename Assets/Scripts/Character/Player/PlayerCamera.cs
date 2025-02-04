7c0ea8b530eb82704101eef11f34340093265650
Duhan Avcı <avciduhan@gmail.com>
2025-02-04 23:23:55 +0300

    arrangement of folders for locomotion

--- Assets/Scripts/Character/Player/PlayerCamera.cs
+++ Assets/Scripts/Character/Player/PlayerCamera.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
@@ -19,9 +21,11 @@ public class PlayerCamera : MonoBehaviour
     [SerializeField] float minPivot = -30;
     [SerializeField] float maxPivot = 60;
     [SerializeField] float cameraColliderRadius = 0.2f;
+    [SerializeField] LayerMask collideWithLayers;
 
     [Header("Camera Values")]
     private Vector3 cameraVelocity;
+    private Vector3 cameraObjectPosition;
     private float rightAndLeftLookAngle;
     private float upAndDownLookAngle;
     private float cameraZPosition;
@@ -42,7 +46,7 @@ public class PlayerCamera : MonoBehaviour
     private void Start()
     {
         DontDestroyOnLoad(gameObject); // menüler arasý varlýðýný koruyabilsin diye
-
+        cameraZPosition = cameraObject.transform.localPosition.z;
 
     }
 
@@ -90,20 +94,29 @@ public class PlayerCamera : MonoBehaviour
         cameraPivotTransform.localRotation = targetRotation;
 
     }
+    
+
     private void HandleCameraCollisions()
     {
+        //bir spherecast gönderilecek çarpýþma olursa hit pointle oyuncu arasýndaki mesafeyi hesaplaycak ve bu mesafeden sphere castýmýzýn radiusunu çýkarýcak bu bizim targettimiz olacak
         targetCameraZPosition = cameraZPosition;
 
         RaycastHit hit;
         Vector3 direction = cameraObject.transform.position - cameraPivotTransform.position;
+        direction.Normalize();
 
-        if (Physics.SphereCast(cameraPivotTransform.position, cameraColliderRadius, direction, out hit, Mathf.Abs(targetCameraZPosition), 0)){
+        if (Physics.SphereCast(cameraPivotTransform.position,cameraColliderRadius,direction,out hit, Mathf.Abs(targetCameraZPosition), collideWithLayers))//bir çarpýþma gerçekleþirse
+        {
+            float distanceFromHitObject = Vector3.Distance(cameraPivotTransform.position,hit.point);
+            targetCameraZPosition = -(distanceFromHitObject - cameraColliderRadius);//cameramýz parentine göre -3 z de olacak þekilde olduðu için negatif düþünecez 
 
-            float distanceFromHitObject = Vector3.Distance(cameraObject.transform.position,hit.point);
+            if(Mathf.Abs(targetCameraZPosition) < cameraColliderRadius)
+            {
+                targetCameraZPosition = -cameraColliderRadius;
+            }
+
         }
-       
-
+        cameraObjectPosition.z = Mathf.Lerp(cameraObject.transform.localPosition.z, targetCameraZPosition, 0.2f);
+        cameraObject.transform.localPosition = cameraObjectPosition;
     }
-
-
 }
